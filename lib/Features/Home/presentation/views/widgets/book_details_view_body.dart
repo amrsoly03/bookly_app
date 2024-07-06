0ba5b3314274cb5ce09742e965a75806786490cb
Amr <amraeosoly@gmail.com>
2024-07-07 01:22:16 +0300

    create books button action

--- lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
@@ -1,8 +1,10 @@
 import 'package:bookly_app/Core/utils/styles.dart';
+import 'package:bookly_app/Core/widgets/custom_button.dart';
 import 'package:bookly_app/Features/Home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/Features/Home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
+import 'book_action.dart';
 import 'custom_book_details_appbar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -13,39 +15,43 @@ class BookDetailsViewBody extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            children: [
-              const CustomBookDetailsAppBar(),
-              const SizedBox(height: 10),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: width * 0.17),
-                child: const CustomBookItem(),
-              ),
-              const SizedBox(height: 40),
-              Text(
-                'Lord Of The Rings',
-                style: Styles.textStyle30.copyWith(
-                  fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                const CustomBookDetailsAppBar(),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * 0.17),
+                  child: const CustomBookItem(),
                 ),
-              ),
-              const SizedBox(height: 6),
-              Opacity(
-                opacity: 0.7,
-                child: Text(
-                  'R.R Toalken',
-                  style: Styles.textStyle18.copyWith(
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.w500,
+                const SizedBox(height: 40),
+                Text(
+                  'Lord Of The Rings',
+                  style: Styles.textStyle30.copyWith(
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              const SizedBox(height: 18),
-              BookRating(
-                mainAxisAlignment: MainAxisAlignment.center,
-              ),
-            ],
+                const SizedBox(height: 6),
+                Opacity(
+                  opacity: 0.7,
+                  child: Text(
+                    'R.R Toalken',
+                    style: Styles.textStyle18.copyWith(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 18),
+                const BookRating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(height: 37),
+                const BookAction(),
+              ],
+            ),
           ),
         ),
       ),

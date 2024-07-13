ef2091157949c1236f9ae84194e5202c913b9b2c
Amr <amraeosoly@gmail.com>
2024-07-14 01:06:01 +0300

    Newest books cubit done

---
+++ lib/Features/Home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/Home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/Home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(NewestBooksInitial());
+
+    var result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books));
+      },
+    );
+  }
+}

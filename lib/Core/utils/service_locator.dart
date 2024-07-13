0022c379874675ace3425c584bd77ed946d7f40e
Amr <amraeosoly@gmail.com>
2024-07-14 01:43:09 +0300

    setup service locator

---
+++ lib/Core/utils/service_locator.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/Core/utils/api_service.dart';
+import 'package:bookly_app/Features/Home/data/repos/home_repo_impl.dart';
+import 'package:get_it/get_it.dart';
+
+final GetIt getIt = GetIt.instance();
+
+void setupServiceLocator() {
+  getIt.registerSingleton<ApiService>(ApiService());
+
+  getIt.registerSingleton<HomeRepoImpl>(
+    HomeRepoImpl(getIt.get<ApiService>()),
+  );
+}
